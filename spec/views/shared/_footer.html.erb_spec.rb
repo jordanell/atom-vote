166335df9143583d722bcb16dcc3d7adaacdd677
Jordan Ell <jordan.ell7@gmail.com>
2015-07-26 21:48:58 -0700

    Added the footer view.

---
+++ spec/views/shared/_footer.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'shared/_footer.html.erb' do
+  it 'should display the logo' do
+    render partial: 'shared/footer'
+
+    rendered.should have_css("img[alt*='AtomVote']")
+  end
+
+  it 'should display the correct link' do
+    render partial: 'shared/footer'
+
+    ['New Poll', 'Terms of Service', 'Privacy Policy', 'Issues'].each do |link|
+      rendered.should have_selector('a', text: link)
+    end
+  end
+end
