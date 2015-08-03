dfa8829be0b077547c14794de95352e58c2176c3
Jordan Ell <jordan.ell7@gmail.com>
2015-08-03 14:38:20 -0700

    Fix for tests.

--- spec/views/polls/_share.html.erb_spec.rb
+++ spec/views/polls/_share.html.erb_spec.rb
@@ -1,15 +1,27 @@
 require 'rails_helper'
 
 describe 'polls/_share.html.erb' do
-  it 'should display the share text' do
-    render partial: 'polls/share'
+  context 'with poll instance variable' do
+    before(:each) do
+      @poll = FactoryGirl.create(:poll)
+    end
 
-    rendered.should have_content('Share this poll')
+    it 'should display the share text' do
+      render partial: 'polls/share'
+
+      rendered.should have_content('Share this poll')
+    end
+
+    it 'should display the input' do
+      render partial: 'polls/share'
+
+      rendered.should have_selector('input')
+    end
   end
 
-  it 'should display the input' do
-    render partial: 'polls/share'
-
-    rendered.should have_selector('input')
+  context 'without poll instance variable' do
+    it 'should fail to render the template' do
+      expect{render}.to raise_error(ActionView::Template::Error)
+    end
   end
 end
