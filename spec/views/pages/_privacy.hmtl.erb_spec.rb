4de395c11af74a7c76eb1315f0fbd7b62be52bdb
Jordan Ell <jordan.ell7@gmail.com>
2015-08-03 13:35:08 -0700

    Added privacy policy.

--- spec/views/pages/_privacy.hmtl.erb_spec.rb
+++ spec/views/pages/_privacy.hmtl.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'pages/_terms.html.erb' do
+describe 'pages/_privacy.html.erb' do
   it 'should display the title' do
-    render partial: 'pages/terms'
+    render partial: 'pages/privacy'
 
-    rendered.should have_content('Terms of Service')
+    rendered.should have_content('Privacy Policy')
   end
 
   it 'should have a link to strawpoll' do
-    render partial: 'pages/terms'
+    render partial: 'pages/privacy'
 
     rendered.should have_selector('a', text: 'StrawPoll')
   end
