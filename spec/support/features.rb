912b6c941da3332b9681a79c4abd46d80af61ff1
Jordan Ell <jordan.ell7@gmail.com>
2015-08-04 22:28:24 -0700

    Attempting fix for circleCI.

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,3 +1,5 @@
+Dir['spec/supportfeatures/*.rb'].each {|file| require file }
+
 RSpec.configure do |config|
   config.include Features::PollHelpers, type: :feature
   config.include Features::VoteHelpers, type: :feature
