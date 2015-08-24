7f0734b33f1d846681e9ce80c860086b346c0e02
Jordan Ell <jordan.ell7@gmail.com>
2015-08-23 22:00:25 -0700

    Clean up base controller for slack api.

--- app/controllers/api/slack/base_controller.rb
+++ app/controllers/api/slack/base_controller.rb
@@ -1,29 +1,3 @@
 class Api::Slack::BaseController < ActionController::Base
   protect_from_forgery with: :null_session
-
-  before_filter :set_seo_tags
-
-  private
-
-  def set_seo_tags
-    set_meta_tags title: 'AtomVote',
-                  description: 'Create a simple poll to share with the world.',
-                  keywords: %w[Poll Vote Question Survey Atom AtomVote],
-                  nofollow: true,
-                  og:  {
-                    title: 'AtomVote',
-                    type:  'website',
-                    url:   'http://atomvote.com',
-                    image: 'https://s3.amazonaws.com/atomvote-production/meta/atom_vote_logo_64.png',
-                  }
-  end
-
-  def render_error(code)
-    @status_code = code
-
-    respond_to do |format|
-      format.html { render 'errors/show', status: code }
-      format.json { render json: { message: 'An error has occurred' }, status: code }
-    end
-  end
 end
