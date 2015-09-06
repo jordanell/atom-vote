e55a717833a82576163a81ca64ef65f25b94b057
Jordan Ell <jordan.ell7@gmail.com>
2015-09-06 14:40:45 -0700

    Fixing analytics helper tests.

--- spec/helpers/analytics_helper_spec.rb
+++ spec/helpers/analytics_helper_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe AnalyticsHelper do
+  before(:each) do
+    unless GOOGLE_ANALYTICS
+      GOOGLE_ANALYTICS = 'fake-id'
+    end
+  end
+
   describe '#send_event' do
     it 'should return true with category and action' do
       helper.send_event('Object', 'Action').should eq(true)
