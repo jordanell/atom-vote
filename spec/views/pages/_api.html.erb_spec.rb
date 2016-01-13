63ee9dcf2ff11a01b5defe46ddf64f39f35de720
Jordan Ell <jordan.ell7@gmail.com>
2016-01-13 11:47:13 -0800

    Removed old API test.

--- spec/views/pages/_api.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'pages/_api.html.erb' do
-  it 'should display the title' do
-    render partial: 'pages/api'
-
-    rendered.should have_content('API')
-  end
-end
